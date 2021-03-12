f1e2ea62f6208edc479161f138c1fe5f1afd6b39
Christian Prada Osuna <christian@impulso.xyz>
2021-03-12 17:22:30 +0100

    [VER] Add new version

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
 
     # for the full list
     'category': 'Uncategorized',
-    'version': '13.0.1.0.9',
+    'version': '13.0.1.0.10',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'sale'],
