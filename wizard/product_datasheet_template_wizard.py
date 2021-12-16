063a1ef4efb254cddf60268e27d5d67731e26a04
alfonsomb <alfonso@impulso.xyz>
2021-12-16 13:38:43 +0100

    Download Layout

--- wizard/product_datasheet_template_wizard.py
+++ wizard/product_datasheet_template_wizard.py
@@ -6,6 +6,7 @@ from odoo.exceptions import UserError, AccessError
 from datetime import datetime, date
 from io import BytesIO
 import openpyxl
+import xlsxwriter
 import string
 import base64
 import tempfile
@@ -17,7 +18,7 @@ import os
 class ProductDatasheetTemplateWizard(models.TransientModel):
     _name = 'product.datasheet.template.wizard'
 
-    template_id = fields.Many2one('product.datasheet.template', string='Template', required=True)
+    template_id = fields.Many2one('product.datasheet.template', string='Template')
     product_ids = fields.Many2many('product.product', string='Product')
     file_generated = fields.Binary(string='Generated ZIP file', attachment=False)
 
@@ -26,6 +27,8 @@ class ProductDatasheetTemplateWizard(models.TransientModel):
         return {'value': {'file_generated': False}}
 
     def action_download_excel(self):
+        if not self.template_id:
+            raise UserError(_("You don't select a Template!"))
         print(f'You have chosen {self.template_id.name}')
         tmp_url = '/tmp/ProductDatasheetTemplate/'
         try:
@@ -167,6 +170,190 @@ class ProductDatasheetTemplateWizard(models.TransientModel):
         data = None
         with open(tmp_url + 'ProductDatasheetTemplates.zip', 'rb') as f:
             data = f.read()
+        self.file_generated = base64.encodestring(data)
+
+        return {
+            'context': self.env.context,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'product.datasheet.template.wizard',
+            'res_id': self.id,
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
+
+    def action_download_layout(self):
+        filename = f'Layout Delisano.xlsx'
+        output = BytesIO()
+
+        _info = {
+            'code': 'Layout Delisano',
+            'created': datetime.now().strftime('%Y/%m/%d')
+        }
+
+        workbook = xlsxwriter.Workbook(output)
+
+        # TEXT FORMAT
+        bold = workbook.add_format({'bold': True})
+        italic = workbook.add_format({'italic': True})
+        italic.set_font_size(10)
+
+        # CELL FORMAT
+        black_format = workbook.add_format({
+            'bold': True,
+            'font_color': 'white',
+            'bg_color': 'black'
+        })
+        normal_format = workbook.add_format({
+            'font_color': 'black',
+            'bg_color': 'white',
+            'border': 1
+        })
+
+        # TAB NAME
+        worksheet = workbook.add_worksheet(self.display_name)  # Tab with display_name of product
+
+        letter_column = list(string.ascii_uppercase)  # Array from A to Z
+        for letter in letter_column:  # Set width column from A to Z
+            worksheet.set_column(letter + ':' + letter, 25)
+
+        # FIELD -> SECTION AND GROUP IF FIELD IS DUPLICATED INFO PRODUCT
+        fields_to_print = [
+            'DEFAULT_CODE',
+            'DCP',
+            'DPUF',
+            'NP',
+            'DPE',
+            'PA',
+            'DEN',
+            'CTA',
+            'CDVC',
+            'ING',
+            'PRP',
+            'VUC',
+            'VUR',
+            'CH',
+            'CPA',
+            'TEXTO',
+            'CNP',
+            'DPPAL',
+            'DPPAN',
+            'DPPL',
+            'VE1-IN-VM100',
+            'VE2-IN-VM100',
+            'GR-IN-VM100',
+            'GRS-IN-VM100',
+            'CAH-IN-VM100',
+            'CAHG-IN-VM100',
+            'FA-IN-VM100',
+            'PR-IN-VM100',
+            'NA-IN-VM100',
+            'K-IN-VM100',
+            'FE-IN-VM100',
+            'MG-IN-VM100',
+            'CA-IN-VM100',
+            'P-IN-VM100',
+            'ZN-IN-VM100',
+            'VB12-IN-VM100',
+            'GLU-AOI-ALD',
+            'GLU-AOI-ALI',
+            'CRU-AOI-ALD',
+            'CRU-AOI-ALI',
+            'HUE-AOI-ALD',
+            'HUE-AOI-ALI',
+            'PES-AOI-ALD',
+            'PES-AOI-ALI',
+            'CAC-AOI-ALD',
+            'CAC-AOI-ALI',
+            'SOJ-AOI-ALD',
+            'SOJ-AOI-ALI',
+            'LEC-AOI-ALD',
+            'LEC-AOI-ALI',
+            'FRU-AOI-ALD',
+            'FRU-AOI-ALI',
+            'API-AOI-ALD',
+            'API-AOI-ALI',
+            'MOS-AOI-ALD',
+            'MOS-AOI-ALI',
+            'GRA-AOI-ALD',
+            'GRA-AOI-ALI',
+            'DIO-AOI-ALD',
+            'DIO-AOI-ALI',
+            'LUP-AOI-ALD',
+            'LUP-AOI-ALI',
+            'RAM-AM-M',
+            'MOL-AOI-ALD',
+            'MOL-AOI-ALI',
+            'STA-AM-M',
+            'ENT-AM-M',
+            'LIS-AM-M',
+            'BAC-AM-M',
+            'LEV-AM-M',
+            'MOH-AM-M',
+            'SALM-AM-M',
+            'BACA-AM-M',
+            'ECOL-AM-M',
+            'MDP1'
+        ]
+
+        pdinfo_obj = self.env['product.datasheet.info']
+        pdsection_obj = self.env['product.datasheet.section']
+        pdgroup_obj = self.env['product.datasheet.group']
+        pdfield_obj = self.env['product.datasheet.field']
+
+        i = 0
+        j = 0
+        for key in fields_to_print:
+            if j == 0:
+                worksheet.write(i, j, 'Default Code', black_format)
+            else:
+                key_edit = key.split('-')[0] if '-' in key else key
+                pdfield = pdfield_obj.search([('code', '=', key_edit)])
+                if pdfield:
+                    name_column = ''
+                    name_column += pdgroup_obj.search([('code', '=', 'ALD')],
+                                                      limit=1).name + ' ' if 'ALD' in key else ''
+                    name_column += pdgroup_obj.search([('code', '=', 'ALI')],
+                                                      limit=1).name + ' ' if 'ALI' in key else ''
+                    name_column += pdfield.name
+                    name_column += ' (' + pdfield.uom + ')' if pdfield.uom else ''
+                    worksheet.write(i, j, name_column, black_format)
+            j += 1
+
+        i += 1
+        for product in self.product_ids:
+            j = 0
+            for key in fields_to_print:
+                if j == 0:
+                    worksheet.write(i, j, product.default_code, normal_format)
+                else:
+                    key_edit = key.split('-')[0] if '-' in key else key
+                    pdfield = pdfield_obj.search([('code', '=', key_edit)])
+                    if pdfield:
+                        print(pdfield.code)
+                        if '-' in key:
+                            pdsection = pdsection_obj.search([('code', '=', key.split('-')[1])])
+                            pdgroup = pdgroup_obj.search([('code', '=', key.split('-')[2])])
+                            if pdsection and pdgroup:
+                                pdinfo = pdinfo_obj.search([('product_id', '=', product.id),
+                                                            ('section_id', '=', pdsection.id),
+                                                            ('group_id', '=', pdgroup.id),
+                                                            ('field_id', '=', pdfield.id)])
+                        else:
+                            pdinfo = pdinfo_obj.search([('product_id', '=', product.id),
+                                                        ('field_id', '=', pdfield.id)])
+
+                        worksheet.write(i, j, pdinfo.value_display if pdinfo else '', normal_format)
+                j += 1
+            print(product.name)
+            i += 1
+
+        print('Saving excel...')
+        workbook.close()
+        output.seek(0)
+
+        data = output.read()
         self.file_generated = base64.encodestring(data)
 
         return {
