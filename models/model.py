23681182a0752635414addc16e1c1dbe92ba4d28
alfonsomb <alfonso@impulso.xyz>
2022-04-25 11:38:55 +0200

    Warning to Values of Selection Field

--- models/model.py
+++ models/model.py
@@ -239,11 +239,28 @@ class Info(models.Model):
     def create(self, vals):
         res = super(Info, self).create(vals)
         if 'value' in vals and res.field_id and res.field_id.related_field_product_id:
+            if res.field_id.related_field_product_id.ttype == 'selection':
+                values_selection = list(
+                    set(res.field_id.related_field_product_id.selection.replace('[', '').replace(']', '').replace('(',
+                                                                                                                  '').replace(
+                        ')', '').replace(' ', '').replace("'", '').split(',')))
+                if vals['value'] not in values_selection:
+                    raise UserError(_('%s value does not exist! Allowed values are %s\n\nContact support') % (
+                    vals['value'], values_selection,))
             res.product_id.product_tmpl_id.write({res.field_id.related_field_product_id.name: vals['value']})
         return res
 
     def write(self, values):
         if 'value' in values and self.field_id and self.field_id.related_field_product_id:
+            if self.field_id.related_field_product_id.ttype == 'selection':
+                values_selection = list(
+                    set(self.field_id.related_field_product_id.selection.replace('[', '').replace(']', '').replace('(',
+                                                                                                                   '').replace(
+                        ')', '').replace(' ', '').replace("'", '').split(',')))
+                if values['value'] not in values_selection:
+                    raise UserError(
+                        _('%s value does not exist! The allowed values for this field are %s\n\nContact support') % (
+                        values['value'], values_selection,))
             self.product_id.product_tmpl_id.write({self.field_id.related_field_product_id.name: values['value']})
         return super(Info, self).write(values)
 
@@ -365,7 +382,8 @@ class ProductProduct(models.Model):
         return res
 
     x_studio_presentacin = fields.Char(related='product_tmpl_id.x_studio_presentacin', string='Presentation')
-    x_studio_presentacin_usuario_final = fields.Char(related='product_tmpl_id.x_studio_presentacin_usuario_final', string='Presentation Final User')
+    x_studio_presentacin_usuario_final = fields.Char(related='product_tmpl_id.x_studio_presentacin_usuario_final',
+                                                     string='Presentation Final User')
 
     info_ids = fields.One2many('product.datasheet.info', 'product_id', domain=filter_by_name)
     image_ids = fields.One2many('product.datasheet.image', 'product_id')
